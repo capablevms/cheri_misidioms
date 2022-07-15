e4f991878d9a3ea2ba038d7821c0937a01c08af4
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-07-15 14:31:23 +0100

    Add some text to Section 3
    * try to provide an example of simple bump allocator;
    * some points about allocator/gc features;
    * add a macro for function names.

---
+++ code/bump_alloc.c
@@ -0,0 +1,12 @@
+void* heap_start = 0x100000UL;
+size_t allocated = 0;
+
+void* alloc(size_t size) {
+    void* new_alloc = mmap(heap_start + allocated, size);
+    allocated += size;
+    return new_alloc; }
+
+void* realloc(void* ptr, size_t size) {
+    void* new_alloc = alloc(size);
+    memcpy(ptr, new_alloc, ptr->get_bounds());
+    return new_alloc; }
