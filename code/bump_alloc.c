d12410a95a6f425c3e15241bfe4096a913490adb
Laurence Tratt <laurie@tratt.net>
2022-07-19 17:15:36 +0100

    Some edits to section 3.

--- code/bump_alloc.c
+++ code/bump_alloc.c
@@ -1,12 +1,14 @@
 void* heap_start = 0x100000UL;
 size_t allocated = 0;
 
-void* alloc(size_t size) {
+void* malloc(size_t size) {
     void* new_alloc = mmap(heap_start + allocated, size);
     allocated += size;
     return new_alloc; }
 
 void* realloc(void* ptr, size_t size) {
-    void* new_alloc = alloc(size);
+    void* new_alloc = malloc(size);
     memcpy(ptr, new_alloc, ptr->get_bounds());
     return new_alloc; }
+
+void free(void *ptr) { }
