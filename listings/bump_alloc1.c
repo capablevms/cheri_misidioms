36792824f26cb909dbf6711ded1b62da82bad46c
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-07-26 15:01:50 +0100

    Improve bump allocator example

--- listings/bump_alloc1.c
+++ listings/bump_alloc1.c
@@ -1,9 +1,14 @@
-void* heap_start = 0x100000UL;
+void* heap_start = NULL;
 size_t allocated = 0;
 
 void* malloc(size_t size) {
-    void* new_alloc = mmap(heap_start + allocated, size);
+    if (heap_start == NULL) {
+        heap_start = mmap(NULL, 0x1000000UL,
+                PROT_READ | PROT_WRITE,
+                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    }
+    void* new_ptr = heap_start + allocated;
     allocated += size;
-    return new_alloc; }
+    return new_ptr;
 
 void free(void *ptr) { }
