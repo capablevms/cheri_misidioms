0510b3c1f4000a91d564854662dbf6562fafb8a4
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2022-08-02 16:20:58 +0100

    made bump allocator more complicated!

--- listings/bump_alloc1.c
+++ listings/bump_alloc1.c
@@ -1,14 +1,24 @@
-void* heap_start = NULL;
-size_t allocated = 0;
+void* heap_start = NULL; // FIXME - do we need to retain start address?
+void* heap_current = NULL; // the bump pointer
+void* heap_end = NULL;
+size_t HEAP_SIZE = 0x1000000UL;
 
 void* malloc(size_t size) {
+    void* new_ptr = NULL;
     if (heap_start == NULL) {
-        heap_start = mmap(NULL, 0x1000000UL,
+        // first call of malloc - init
+        heap_start = mmap(NULL, HEAP_SIZE,
                 PROT_READ | PROT_WRITE,
                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	heap_current = heap_start;
+	heap_end = heap_start + HEAP_SIZE;
     }
-    void* new_ptr = heap_start + allocated;
-    allocated += size;
+    // FIXME - should we check mmap returns valid pointer?
+    if (heap_current+size < heap_end) {
+      new_ptr = heap_current;
+      heap_current += size; // bump
+    }
     return new_ptr;
+}
 
 void free(void *ptr) { }
