9ec859b1ca52c7e3467595a590e82bef0cd69670
Andrei Lascu <andrei.lascu@kcl.ac.uk>
2022-10-04 16:24:20 +0100

    Listing fixes
    * Change undeclared variable use in `bump_alloc2.c`

--- listings/bump_alloc2.c
+++ listings/bump_alloc2.c
@@ -1,4 +1,3 @@
-
 void* malloc(size_t size) {
     void* new_ptr = NULL;
     void* tmp_ptr = NULL; /* for CHERI alignment */
@@ -9,12 +8,11 @@ void* malloc(size_t size) {
     // align pointer and size for CHERI
     tmp_ptr = __builtin_align_up(heap_current,
         ~cheri_representable_alignment_mask(size) + 1);
-    rounded_size = cheri_representable_length(size);
+    rounded_len = cheri_representable_length(size);
 
-    
-    if (tmp_ptr+rounded_size < heap_end) {
+    if (tmp_ptr+rounded_len < heap_end) {
       // enough space to satisfy malloc
-      heap_current = tmp_ptr+rounded_size; // bump
+      heap_current = tmp_ptr+rounded_len; // bump
       new_ptr = cheri_bounds_set_exact(tmp_ptr, rounded_len); // restrict
     }
 
