b754d07ee9198c3f58545a8d8d4fae095f95079c
Jacob Bramley <jacob.bramley@arm.com>
2023-03-01 12:19:37 +0000

    Small microbenchmark improvements.
    - Remove some spurious variation in 99-busy-loop.
    - Print results as CSV.

--- code/microbenchmarks/99-busy-loop.c
+++ code/microbenchmarks/99-busy-loop.c
@@ -11,14 +11,20 @@
 
 #include "cli.h"
 
+// Prevent inlining, so we don't have to consider secondary effects (such as
+// loop alignment).
+uint64_t __attribute__((noinline)) loop(uint64_t count) {
+    uint64_t i;
+    for (i = 0; i < count; i++) {
+        __asm__ volatile("" : : : "memory");
+    }
+    return i;
+}
+
 int main(int argc, char const * argv[]) {
     struct Args args = process_args(argc, argv);
     before_test(&args);
-    uint64_t i;
-    uint64_t count = args.fast ? 42 : 4200000000;
-    for (i = 0; i < count; i++) {
-        __asm__ volatile("");
-    }
+    uint64_t count = loop(args.fast ? 42 : 4200000000);
     after_test(&args);
-    printf("Busy-looped %" PRIu64 " times.\n", i);
+    printf("Busy-looped %" PRIu64 " times.\n", count);
 }
--- code/microbenchmarks/run-remote.sh
+++ code/microbenchmarks/run-remote.sh
@@ -10,10 +10,16 @@ set -eu
 SSHHOST="${SSHHOST:?SSHHOST is unset (or empty)}"
 dst="capablevms/microbenchmarks"
 
-ssh "$SSHHOST" mkdir -p "$dst"
-scp -r bin/ "$SSHHOST":"$dst/"
+ssh -q "$SSHHOST" mkdir -p "$dst"
+scp -q -r bin/ "$SSHHOST":"$dst/"
 
-for bin in bin/*; do
-    echo "Running $bin..."
-    ssh -t "$SSHHOST" time "$dst/$bin"
+echo "Set,Benchmark,Result"
+for n in $(seq 1 20); do
+    for bin in bin/*; do
+        name="$(basename $bin)"
+        set_name="${name##*-}"
+        bm_name="${name%-*}"
+        user=$(ssh -q -t "$SSHHOST" time "$dst/$bin" | grep -P --only-matching '\b[0-9]+\.[0-9]+(?= user)')
+        echo "$set_name,$bm_name,$user"
+    done
 done
