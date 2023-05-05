c347fb0e481148cd465bb127f8b34fe220d88445
Jacob Bramley <jacob.bramley@arm.com>
2023-05-05 19:39:11 +0100

    Sort microbenchmarks to match prose.

--- code/microbenchmarks/plot.sh
+++ code/microbenchmarks/plot.sh
@@ -17,8 +17,20 @@ out="$(dirname "${BASH_SOURCE[0]}")"/../../fig/microbenchmarks.pdf
 # Skip 21-global and 22-global-other-so; they don't measure anything new and the
 # names promise more than they actually offer.
 grep -v '\<2.-global' results.csv |
+    # Add temporary sort keys to match the order in the text.
+    sed -e 's/^Set,Benchmark,Result$/00-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-random-graph-walk-l1/10-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-random-graph-walk-fixed/11-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-ptr-add-asm/12-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-busy-loop/13-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-factorial-asm-minimal/20-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-factorial-asm-indirect/21-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-so-call/22-&/' |
+    sed -e 's/\(hybrid\|purecap\),[0-9][0-9]-ptr-add-align/23-&/' |
+    sort --stable --field-separator=- --key 1,1 |
     # Drop number prefixes, to save space (and match the text).
-    sed -e 's/\<[0-9][0-9]-//' |
+    # This also removes the temporary sort keys.
+    sed -e 's/\<[0-9][0-9]-//g' |
     # l1 -> L1, to match the text.
     sed -e 's/-l1\>/-L1/' |
     "$BMPLOTTER" --width=0.5 --height=0.7 --no-legend-title --colours=759DA0,E1A765,8A67BA,E06A4A --out="$out" --overplot=jitter
